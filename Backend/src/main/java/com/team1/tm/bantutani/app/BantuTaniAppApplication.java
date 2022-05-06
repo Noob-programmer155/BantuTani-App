c54da1be767702825a1297157d349c25260848dc
Amar <Amr.java8dev321@gmail.com>
2022-05-06 14:36:17 +0700

    first commit CC

---
+++ Backend/src/main/java/com/team1/tm/bantutani/app/BantuTaniAppApplication.java
@@ -0,0 +1,13 @@
+package com.team1.tm.bantutani.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantuTaniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BantuTaniAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/team1/tm/bantutani/app/BantuTaniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.tm.bantutani.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantuTaniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
