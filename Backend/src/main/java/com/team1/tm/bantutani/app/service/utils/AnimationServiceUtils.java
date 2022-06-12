9359799a29b8ea71a9bef005e551de7aff688da0
Amar <Amr.java8dev321@gmail.com>
2022-06-12 09:16:24 +0700

    fixing ref variable

--- Backend/src/main/java/com/team1/tm/bantutani/app/service/utils/AnimationServiceUtils.java
+++ Backend/src/main/java/com/team1/tm/bantutani/app/service/utils/AnimationServiceUtils.java
@@ -1,13 +1,11 @@
 package com.team1.tm.bantutani.app.service.utils;
 
 import com.team1.tm.bantutani.app.configuration.StorageConfig;
-import com.team1.tm.bantutani.app.dto.response.AnimationResponseDTOPageable;
 import com.team1.tm.bantutani.app.model.Animation;
 import com.team1.tm.bantutani.app.model.other.AnimationType;
 import com.team1.tm.bantutani.app.repository.AnimationRepo;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
